c28a0e965481bd7ceecc9db0791b43df812f4086
Miriam Martinez <103117556+Mirii05@users.noreply.github.com>
2026-08-13 17:56:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const contenedorPrincipal = document.getElementById("contenedorPrincipal");
+const botonBuscarID = document.getElementById("botonBuscarID");
+const botonLimpiar = document.getElementById("botonLimpiar");
+const mostrarDatosUsuario = document.getElementById("mostrarDatosUsuario");
+const mensajeError = document.getElementById("mensajeError");
+const inputID = document.getElementById("ingresaID");
+//----------------funciones----------------
+function ingresaBuscaID(){
+    console.log("se hizo clic en buscar");
+    const idIngresado = inputID.value;
+    if (idIngresado === ""){
+        mensajeError.textContent = "Por favor ingresa un ID válido";
+        return;
+    }
+    console.log("ID ingresado:", idIngresado);
+    return idIngresado;
+}
+
+
+async function obtenerID(idIngresado) {
+    try{
+        const informacion = await fetch("https://jsonplaceholder.typicode.com/users/" + idIngresado);
+        if(informacion.ok){
+            console.log("La petición fue exitosa");
+
+            const datos = await informacion.json();
+        }else{
+            console.log("La petición falló");
+            console.log("Estatus:", informacion.status);
+        }
+    }catch (error){
+        console.log("Ocurrió un error: ", error);
+    }
+    return datos;
+}
+function mostrarInformacion(datos){
+    mostrarDatosUsuario.innerHTML = `
+    <p>Nombre: ${datos.name}</p>
+    <p>Usuario: ${datos.username}</p>
+    <p>Email: ${datos.email}</p>
+    <p>Dirección</p>
+    <p>Calle: ${datos.address.street}</p>
+    <p>Ciudad: ${datos.address.city}</p>
+    <p>Código postal: ${datos.address.zipcode}</p>`;
+}
+//----------------eventos-----------------
+botonBuscarID.addEventListener("click", ingresaBuscaID);
+obtenerID(idIngresado);
+mostrarInformacion(datos);
+
